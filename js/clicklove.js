7f2a8927957567f51e5c13c3e3e18168230dd437
CUSTzt <45364609+CUSTzt@users.noreply.github.com>
2019-07-30 19:49:28 +0800

    Site updated: 2019-07-30 19:49:27

--- js/clicklove.js
+++ js/clicklove.js
@@ -1,23 +1,31 @@
-<script type="text/javascript">
-$(document).ready(function(){
-    var a_index = 0;
-    $("body").click(function(e){
-        var a = new Array("�ۇ�", "�M��", "�ƌW", "����", "�ԏ���Ϣ","����d��","�ˮ˼Դ","�ۇ��sУ","���W���V־","�І�����˼","����","����","�t���K�M","�팍����","talk is teap","show me code");
-        var $i = $("<span/>").text(a[a_index]);
-        a_index = (a_index + 1) % a.length;
-        var x = e.pageX,y = e.pageY;
-        $i.css({
-            "z-index": 99999,
-            "top": y - 20,
-            "left": x,
-            "position": "absolute",
-            "font-weight": "bold",
-            "color": "#ff6651"
+
+var a_idx = 0;
+ 
+        $("body").click(function(e) {
+            var a = new Array(
+                "Talk is cheap", "Show me code", "自強不息", "厚德載物",
+                "愛國", "進步", "民主", "科學",
+                "求是創新", "飲水思源", "愛國榮校", "博學而篤志","切問而近思"
+                );
+            var $i = $("<span/>").text(a[a_idx]);
+            a_idx = (a_idx + 1) % a.length;
+            var x = e.pageX,
+            y = e.pageY;
+            $i.css({
+                "z-index": 144469,
+                "top": y - 20,
+                "left": x,
+                "position": "absolute",
+                "font-weight": "bold",
+                "color": "#f00"
+            });
+            $("body").append($i);
+            $i.animate({
+                "top": y - 180,
+                "opacity": 0
+            },
+            1500,
+            function() {
+                $i.remove()
+            })
         });
-        $("body").append($i);
-        $i.animate({"top": y-180,"opacity": 0},1500,function() {
-            $i.remove();
-        });
-    });
-});
-</script>
